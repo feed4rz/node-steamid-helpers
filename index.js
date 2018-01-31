dc4de64330d107624c2e373f9fd4e1dcb166c6f7
Matvey <mrspanky@yandex.ru>
2018-01-31 20:22:24 +0300

    Code format

--- index.js
+++ index.js
@@ -1,26 +1,26 @@
-let SteamIDtoPartnerID = (steamid) => {
+const SteamIDtoPartnerID = steamid => {
   return steamid.substr(3) - 61197960265728;
 }
 
-let PartnerIDtoSteamID = (partnerid) => {
+const PartnerIDtoSteamID = partnerid => {
   return "765" + (Number(partnerid) + 61197960265728);
 }
 
-let TradelinkToSteamID = (tradelink) => {
-  let steamid = PartnerIDtoSteamID(tradelink.substr(51, tradelink.length - 66));
+const TradelinkToSteamID = tradelink => {
+  const steamid = PartnerIDtoSteamID(tradelink.substr(51, tradelink.length - 66));
 
   return steamid;
 }
 
-let TradelinkToPartnerID = (tradelink) => {
-  let steamid = tradelink.substr(51, tradelink.length - 66);
+const TradelinkToPartnerID = tradelink => {
+  const steamid = tradelink.substr(51, tradelink.length - 66);
 
   return steamid;
 }
 
 module.exports = {
-	SteamIDtoPartnerID : SteamIDtoPartnerID,
-	PartnerIDtoSteamID : PartnerIDtoSteamID,
-	TradelinkToSteamID : TradelinkToSteamID,
-	TradelinkToPartnerID : TradelinkToPartnerID
+  SteamIDtoPartnerID,
+  PartnerIDtoSteamID,
+  TradelinkToSteamID,
+  TradelinkToPartnerID
 }
